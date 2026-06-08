1e934371baba41a750e8241d593aa6d0e3645b3e
Ekansh Satsangi <ekanshsatsangi@gmail.com>
2026-06-08 06:13:26 +0530

    chore: initialize Next.js Turbopack dev build artifacts

---
+++ .next/dev/static/chunks/[project]_Desktop_medisaar_pages__error_0du2_q-._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/1z37_next_dist_compiled_1n9lykh._.js","static/chunks/1z37_next_dist_shared_lib_04y4_h6._.js","static/chunks/1z37_next_dist_client_0bbkx2c._.js","static/chunks/1z37_next_dist_1cywjlw._.js","static/chunks/10hs_next_dist_pages__error_01acw05.js","static/chunks/[next]_entry_page-loader_ts_1iibg3o._.js","static/chunks/1z37_react-dom_0uk3us8._.js","static/chunks/1z37_08kvymi._.js","static/chunks/[root-of-the-server]__1deeadv._.js"],
+    source: "entry"
+});
---
+++ .next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/1z37_next_dist_compiled_1n9lykh._.js",
+  "static/chunks/1z37_next_dist_shared_lib_04y4_h6._.js",
+  "static/chunks/1z37_next_dist_client_0bbkx2c._.js",
+  "static/chunks/1z37_next_dist_1cywjlw._.js",
+  "static/chunks/10hs_next_dist_pages__error_01acw05.js",
+  "static/chunks/[next]_entry_page-loader_ts_1iibg3o._.js",
+  "static/chunks/1z37_react-dom_0uk3us8._.js",
+  "static/chunks/1z37_08kvymi._.js",
+  "static/chunks/[root-of-the-server]__1deeadv._.js",
+  "static/chunks/[project]_Desktop_medisaar_pages__error_0du2_q-._.js",
+  "static/chunks/turbopack-[project]_Desktop_medisaar_pages__error_1tl_s_z._.js"
+])
