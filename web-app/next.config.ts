21eed82c39e6e496a2c2593a14a66795ee650492
Ekansh Satsangi <ekanshsatsangi@gmail.com>
2026-06-16 22:58:16 +0530

    feat: containerize web-app using Docker and configure Next.js for standalone output mode

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   // React Compiler currently causes a TurbopackInternalError on Vercel
   // reactCompiler: true,
+  output: "standalone",
 };
 
 export default nextConfig;
